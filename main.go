30ac52b768622b310a195ee58208bacdafed0d14
mcclayac <anthony.mcclay@me.com>
2018-12-23 07:58:05 -0600

    Update control Numbers

--- main.go
+++ main.go
@@ -4,32 +4,34 @@ import "fmt"
 
 func main() {
 
+	//Style #0  infinte loop
+	//for {
+	//	fmt.Printf("Infinitle Loop \n")
+	//}
 
 	//Style #1
 	var counter int
 	counter = 0
-	for counter < 10 {          //  while loop
-		fmt.Printf("Hello World!\n")
-		counter ++
+	for counter < 10 { //  while loop
+		fmt.Printf("#1 Hello World! counter: %d\n", counter)
+		counter++
 	}
 
 	//Style #2
-	for counter2 := 0 ; counter2 < 10 ; counter2++ {
-		fmt.Printf("Hello World! version 2 \n")
+	for counter2 := 0; counter2 < 10; counter2++ {
+		fmt.Printf("#2 Hello World! counter: %d\n \n", counter2)
 	}
 
-
-
 	//Style #3  multiple initiation variables
-	for i, j := 0, 1; i < 10  ; i, j = i+1, j*2 {
-		fmt.Printf("i = %d\t j = %d\n", i, j)
+	for i, j := 0, 1; i < 10; i, j = i+1, j*2 {
+		fmt.Printf("#3  two vaiables :  i = %d\t j = %d\n", i, j)
 	}
 
 	// Style $4  while loop boolean
 	counter = 0
 	var stop bool
 	for !stop {
-		fmt.Printf("for/while loop count : %d\n", counter)
+		fmt.Printf("#4 for/while loop count : %d\n", counter)
 		counter++
 		if counter == 10 {
 			stop = true
